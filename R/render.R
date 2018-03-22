9f522211d328be41877ffd6942bf49a2a7f2d1c1
Javier Luraschi <javierluraschi@hotmail.com>
2018-03-21 17:57:17 -0700

    add latest d3.js and support to embed d3 in widget

--- R/render.R
+++ R/render.R
@@ -1,15 +1,35 @@
-#' <Add Title>
+#' Render Data with D3
 #'
-#' <Add Description>
+#' Renders a data with D3 as an HtmlWidget using a generic D3.js script.
+#'
+#' @param data The data to be passed to D3.js script, currently only data frames supported.
+#' @param script The 'JavaScript' file containing the D3.js script.
+#' @param width The desired width of the widget.
+#' @param height The desired height of the widget.
 #'
 #' @import htmlwidgets
 #'
 #' @export
-d3 <- function(message, width = NULL, height = NULL, elementId = NULL) {
+d3_render <- function(
+  data = islands,
+  script = system.file("samples/bubbles.js", package = "d3"),
+  width = NULL,
+  height = NULL,
+  version = "5.0.0")
+{
+  
+  # convert to data frames
+  df <- data
+  if (!is.data.frame(df)) {
+    df <- as.data.frame(df)
+    if (length(row.names(df)) > 0 && !"names" %in% colnames(df)) {
+      df$names <- rownames(df)
+    }
+  }
 
   # forward options using x
-  x = list(
-    message = message
+  x <- list(
+    message = df
   )
 
   # create widget
@@ -19,7 +39,15 @@ d3 <- function(message, width = NULL, height = NULL, elementId = NULL) {
     width = width,
     height = height,
     package = 'd3',
-    elementId = elementId
+    dependencies = htmltools::htmlDependency(
+      "d3",
+      version,
+      system.file(
+        file.path("d3", version),
+        package = "d3"
+      ),
+      script = "d3.js"
+    )
   )
 }
 
@@ -40,13 +68,15 @@ d3 <- function(message, width = NULL, height = NULL, elementId = NULL) {
 #' @name d3-shiny
 #'
 #' @export
-d3Output <- function(outputId, width = '100%', height = '400px'){
+d3Output <- function(outputId, width = '100%', height = '400px')
+{
   htmlwidgets::shinyWidgetOutput(outputId, 'd3', width, height, package = 'd3')
 }
 
 #' @rdname d3-shiny
 #' @export
-renderD3 <- function(expr, env = parent.frame(), quoted = FALSE) {
+renderD3 <- function(expr, env = parent.frame(), quoted = FALSE)
+{
   if (!quoted) { expr <- substitute(expr) } # force quoted
   htmlwidgets::shinyRenderWidget(expr, d3Output, env, quoted = TRUE)
 }
