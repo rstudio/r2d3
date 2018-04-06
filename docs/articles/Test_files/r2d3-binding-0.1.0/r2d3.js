5c806ee5b5f762921fed9dfbf896fde2e9f5bc2f
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-06 10:45:12 -0700

    rebuild site

---
+++ docs/articles/Test_files/r2d3-binding-0.1.0/r2d3.js
@@ -0,0 +1,39 @@
+HTMLWidgets.widget({
+  name: 'r2d3',
+  type: 'output',
+  factory: function(el, width, height) {
+
+    var r2d3 = new R2D3();
+
+    return {
+      renderValue: function(x) {
+        if (!r2d3.root) {
+          r2d3.setVersion(x.version);
+          r2d3.addScript(x.script);
+          r2d3.addStyle(x.style);
+          
+          var root = r2d3.d3().select(el).append(x.container)
+            .attr("width", width)
+            .attr("height", height);
+            
+          r2d3.setRoot(root);
+        }
+        
+        r2d3.setX(x);
+        r2d3.setWidth(width);
+        r2d3.setHeight(height);
+        
+        d3Script(r2d3.d3(), r2d3);
+      },
+
+      resize: function(width, height) {
+        r2d3.root
+          .attr("width", width)
+          .attr("height", height);
+        
+        // TODO: If has resize then resize otherwise d3Script()
+        r2d3.resize(width, height);
+      }
+    };
+  }
+});
