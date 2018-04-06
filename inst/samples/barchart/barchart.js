bd3f2f2b512ba352badf98c7643df81be9d8eeab
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-06 14:36:31 -0700

    make barchart resizeable

--- inst/samples/barchart/barchart.js
+++ inst/samples/barchart/barchart.js
@@ -1,8 +1,11 @@
+// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
+
+var barHeight = Math.floor(r2d3.height / r2d3.data.length);
 r2d3.svg.selectAll('rect')
     .data(r2d3.data)
   .enter()
     .append('rect')
-      .attr('width', function(d) { return d * 10; })
-      .attr('height', '20px')
-      .attr('y', function(d, i) { return i * 22; })
+      .attr('width', function(d) { return d * r2d3.width; })
+      .attr('height', barHeight)
+      .attr('y', function(d, i) { return i * barHeight; })
       .attr('fill', 'steelblue');
