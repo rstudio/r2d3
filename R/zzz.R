f19529106b9561022cd7b08b0c71f4e0408534f0
jjallaire <jj@rstudio.com>
2018-04-10 09:26:24 -0400

    some R CMD check work

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onLoad <- function(...) {
-  if ("knitr" %in% installed.packages()) {
+  if (requireNamespace("knitr", quietly = TRUE)) {
     knit_engines <- get("knit_engines", envir = asNamespace("knitr"))
-    
     knit_engines$set(d3 = knit_d3)
   }
 }
