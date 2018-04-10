641ee6384412fa20ebf3e36c7500edb27b974eb7
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-10 10:09:02 -0700

    fix readme examples

--- inst/samples/barchart/barchart.js
+++ inst/samples/barchart/barchart.js
@@ -1,11 +1,11 @@
 // !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
 
-var barHeight = Math.floor(r2d3.height / r2d3.data.length);
+var barHeight = Math.floor(height / data.length);
 svg.selectAll('rect')
     .data(data)
   .enter()
     .append('rect')
-      .attr('width', function(d) { return d * r2d3.width; })
+      .attr('width', function(d) { return d * width; })
       .attr('height', barHeight)
       .attr('y', function(d, i) { return i * barHeight; })
       .attr('fill', 'steelblue');
