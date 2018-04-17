d1930301904c68c6cef15609532f59b95fc9efe7
jjallaire <jj@rstudio.com>
2018-04-17 19:29:50 -0400

    article on development and debugging

--- docs/articles/gallery_files/r2d3-binding-0.1.1/lib/r2d3-render.js
+++ docs/articles/gallery_files/r2d3-binding-0.1.1/lib/r2d3-render.js
@@ -11,6 +11,8 @@ function R2D3(el, width, height) {
   self.resizer = null;
   self.renderer = null;
   self.rendererDefaut = true;
+  self.captureErrors = null;
+  self.theme = {};
   
   self.setX = function(newX) {
     x = newX;
@@ -18,6 +20,10 @@ function R2D3(el, width, height) {
     
     if (x.type == "data.frame") {
       self.data = HTMLWidgets.dataframeToD3(self.data);
+    }
+    
+    if (x.theme) {
+      self.theme = x.theme;
     }
     
     self.options = x.options;
@@ -41,6 +47,9 @@ function R2D3(el, width, height) {
       .attr("width", self.width)
       .attr("height", self.height);
       
+    if (self.theme.background) root.style("background", self.theme.background);
+    if (self.theme.foreground) root.style("fill", self.theme.foreground);
+      
     self.setRoot(root);
   };
   
@@ -63,19 +72,39 @@ function R2D3(el, width, height) {
   
   self.render = function() {
     if (self.renderer === null) return;
-    self.renderer(self.data, self.root, self.width, self.height, self.options);
+    
+    try {
+      self.renderer(self.data, self.root, self.width, self.height, self.options);
+    }
+    catch (err) {
+      self.showError(err, null, null);
+    }
   };
   
   self.resize = function() {
     if (self.resizer === null) return;
-    self.resizer(self.width, self.height);
+    try {
+      self.resizer(self.width, self.height);
+    }
+    catch (err) {
+      self.showError(err, null, null);
+    }
   };
   
   self.addScript = function(script) {
     var el = document.createElement("script");
     el.type = "text/javascript";
     el.text = script;
+    
+    self.captureErrors = function(msg, url, lineNo, columnNo, error) {
+      self.showError({
+          message: msg,
+          stack: null
+        }, lineNo, columnNo);
+    };
+
     document.head.appendChild(el);
+    self.captureErrors = null;
   };
   
   self.addStyle = function(style) {
@@ -110,7 +139,13 @@ function R2D3(el, width, height) {
   
   self.callD3Script = function() {
     var d3Script = self.d3Script;
-    d3Script(self.d3(), self, self.data, self.root, self.width, self.height, self.options);
+    
+    try {
+      d3Script(self.d3(), self, self.data, self.root, self.width, self.height, self.options, self.theme);
+    }
+    catch (err) {
+      self.showError(err, null, null);
+    }
   };
   
   self.widgetRender = function(x) {
@@ -166,5 +201,111 @@ function R2D3(el, width, height) {
     self.setHeight(height);
     
     self.resizeDebounce();
+  };
+  
+  var openSource = function(filename, line, column) {
+    if (window.parent.postMessage) {
+      window.parent.postMessage({
+        message: "openfile",
+        source: "r2d3",
+        file: filename,
+        line: line,
+        column: column
+      }, window.location.origin);
+    }
+  };
+  
+  var stackExpanded = false;
+  
+  self.showError = function(error, line, column) {
+    var message = error, callstack = "";
+    
+    if (error.message) message = error.error;
+    if (error.stack) callstack = error.stack;
+    
+    if (line === null || column === null) {
+      var reg = new RegExp("at [^\\n]+ \\(<anonymous>:([0-9]+):([0-9]+)\\)");
+      var matches = reg.exec(callstack);
+      if (matches && matches.length === 3) {
+        line = parseInt(matches[1]);
+        column = parseInt(matches[2]);
+      }
+    }
+    
+    var location = null;
+    var lines = x.script.split("\n");
+    var fileLine = null;
+    var header = "// R2D3 Source File: ";
+    for (var maybe = line; line && maybe >= 0; maybe--) {
+      if (lines[maybe].includes(header)) {
+        var data = lines[maybe].split(header)[1];
+        var source = data.split(":")[0].trim();
+        var offset = data.split(":")[1];
+        fileLine = (line - (maybe + 1) + parseInt(offset));
+        location = source;
+      }
+    }
+    
+    if (location) {
+      message = message + " in ";
+    }
+    
+    var container = document.getElementById("r2d3-error-container");
+    if (!container) {
+      container = document.createElement("div");
+      el.appendChild(container);
+    }
+    
+    container.id = "r2d3-error-container";
+    container.innerHTML = "Error: " + message;
+    container.style.fontFamily = "'Lucida Sans', 'DejaVu Sans', 'Lucida Grande', 'Segoe UI', Verdana, Helvetica, sans-serif, serif";
+    container.style.fontSize = "9pt";
+    container.style.border = "solid 1px #CCCCCC";
+    container.style.padding = "5px";
+    container.style.margin = "10px";
+    container.style.background = "#FEFEFE";
+    container.style.color = "#444";
+    container.style.position = "absolute";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.right = "0";
+    
+    if (location) {
+      var linkEl = document.createElement("a");
+      linkEl.innerHTML = location + "#" + fileLine + ":" + column;
+      linkEl.href = "#";
+      linkEl.onclick = function() {
+        openSource(location, fileLine, column);
+      };
+      container.appendChild(linkEl);
+    }
+    
+    if (callstack) {
+      var expand = document.createElement("a");
+      expand.innerHTML = "stack";
+      expand.href = "#";
+      expand.style.float = "right";
+      if (!stackExpanded) container.appendChild(expand);
+      
+      var stack = document.createElement("code");
+      stack.innerHTML = callstack;
+      stack.style.marginTop = "10px";
+      stack.style.display = stackExpanded ? "block" : "none";
+      container.appendChild(stack);
+      
+      expand.onclick = function() {
+        stack.style.display = "block";
+        expand.style.display = "none";
+        stackExpanded = true;
+      };
+    }
+  };
+  
+  window.onerror = function (msg, url, lineNo, columnNo, error) {
+    if (self.captureErrors) {
+      self.captureErrors(msg, url, lineNo, columnNo, error);
+    }
+    
+    return false;
   };
 }
