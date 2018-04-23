cfaf314cba220bbb52acac2f19296b1672bf4de4
jjallaire <jj@rstudio.com>
2018-04-23 11:36:09 -0400

    add save_d3_png function

--- R/publish.R
+++ R/publish.R
@@ -17,6 +17,9 @@
 #' @param knitrOptions A list of \pkg{knitr} chunk options.
 #' 
 #' @importFrom htmlwidgets saveWidget
+#' 
+#' @seealso [save_d3_png()]
+#' 
 #' @export
 save_d3_html <- function(d3, file, selfcontained = TRUE, libdir = NULL, 
                          background = "white", title = "D3 Visualization", 
@@ -30,4 +33,45 @@ save_d3_html <- function(d3, file, selfcontained = TRUE, libdir = NULL,
     title = title,
     knitrOptions = knitrOptions
   )
+}
+
+#' Save a D3 visualization as a PNG image
+#'
+#' Save a D3 visualization to PNG (e.g. for including in another document).
+#'
+#' @details PNG versions of D3 visualizations are created by displaying them in
+#'   an offscreen web browser and taking a screenshot of the rendered web page.
+#'
+#'   Using the `save_d3_png()` function requires that you install the
+#'   \pkg{webshot} package, as well as the phantom.js headless browser (which
+#'   you can install using the function `webshot::install_phantomjs()`).
+#'
+#' @inheritParams save_d3_html
+#'
+#' @param width	Image width
+#' @param height Image height
+#' @param delay Time to wait before taking screenshot, in seconds. Sometimes a
+#'   longer delay is needed for all assets to display properly.
+#' @param zoom A number specifying the zoom factor. A zoom factor of 2 will
+#'   result in twice as many pixels vertically and horizontally. Note that
+#'   using 2 is not exactly the same as taking a screenshot on a HiDPI (Retina)
+#'   device: it is like increasing the zoom to 200 doubling the height and
+#'   width of the browser window. This differs from using a HiDPI device
+#'   because some web pages load different, higher-resolution images when they
+#'   know they will be displayed on a HiDPI device (but using zoom will not
+#'   report that there is a HiDPI device).
+#'   
+#' @seealso [save_d3_html()]
+#'   
+#' @export
+save_d3_png <- function(d3, file, background = "white", width = 992, height = 744, delay = 0.2, zoom = 1) {
+  tmp_html <- tempfile("save_d3_png", fileext = ".html")
+  on.exit(unlink(tmp_html))
+  save_d3_html(d3, file = tmp_html, background = background)
+  if (requireNamespace("webshot", quietly = TRUE)) {
+    webshot::webshot(tmp_html, file, vwidth = width, vheight = height, delay = delay, zoom = zoom)
+  } else {
+    stop("The webshot package is required to save D3 visualizations to PNG files.")
+  }
+  invisible(file)
 }
