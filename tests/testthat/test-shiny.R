ca1ea6b7a8f979b932a3d2b68f1a3c08b7758867
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-24 13:58:04 -0700

    avoid comparing shinytest screenshots

--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -5,5 +5,5 @@ source("utils.R")
 test_that("r2d3 can be used in shiny applications", {
   skip_on_cran()
   
-  shinytest::expect_pass(shinytest::testApp("barchart"))
+  shinytest::expect_pass(shinytest::testApp("barchart", compareImages = FALSE))
 })
