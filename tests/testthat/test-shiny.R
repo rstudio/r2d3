849c6591ec53cdd83fb6f57bdfd837030c697165
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-24 13:15:22 -0700

    avoid using shiny driver directly

--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -2,16 +2,10 @@ context("knit")
 
 source("utils.R")
 
-test_succeeds("r2d3 can be used in shiny applications", {
+test_that("r2d3 can be used in shiny applications", {
   if (!"shinytest" %in% installed.packages()) {
     skip("Package 'shinytest' not installed.")
   }
   
-  app <- shinytest::ShinyDriver$new("barchart/")
-  app$snapshotInit("shinytest")
-  
-  app$setInputs(bar_max = 1)
-  app$snapshot()
-  app$setInputs(bar_max = 0.2)
-  app$snapshot()
+  shinytest::expect_pass(shinytest::testApp("barchart"))
 })
