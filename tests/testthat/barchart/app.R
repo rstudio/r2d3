f0dea1a5938b0e491e95219b9bca1bef4e5b6cb5
Javier Luraschi <javierluraschi@hotmail.com>
2018-04-24 12:37:19 -0700

    add shiny test

---
+++ tests/testthat/barchart/app.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(r2d3)
+
+ui <- fluidPage(
+  inputPanel(
+    sliderInput("bar_max", label = "Max:",
+                min = 0.1, max = 1.0, value = 0.2, step = 0.1)
+  ),
+  d3Output("d3")
+)
+
+server <- function(input, output) {
+  output$d3 <- renderD3({
+    r2d3(
+      rep(input$bar_max, 5),
+      script = "../baranim.js"
+    )
+  })
+}
+
+shinyApp(ui = ui, server = server)
