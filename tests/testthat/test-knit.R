a07b4fbf2b39d6c47e09dad66f22367034066c95
Barret Schloerke <schloerke@gmail.com>
2021-11-05 11:30:21 -0400

    Remove travis links and skip on travis (works on GHA)

--- tests/testthat/test-knit.R
+++ tests/testthat/test-knit.R
@@ -4,11 +4,10 @@ source("utils.R")
 
 test_succeeds("r2d3 can knit visualizations", {
   skip_on_cran()
-  skip_on_travis()
-  
+
   if (!rmarkdown::pandoc_available())
     skip("rmarkdown requires pandoc")
-  
+
   output <- tempfile(fileext = ".html")
   rmarkdown::render("barchart.Rmd", output_file = output)
 })
