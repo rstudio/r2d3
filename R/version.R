0c2f98abafbe229b584411034de3b2be228658f5
CJ Yetman <cyetman@rmi.org>
2024-01-09 12:46:00 +0100

    Update d3 v6 to use 6.7.0

--- R/version.R
+++ R/version.R
@@ -1,7 +1,7 @@
 # Completes major version to full version
 version_complete <- function(minor) {
   versions <- list(
-    "6" = "6.2.0",
+    "6" = "6.7.0",
     "5" = "5.9.2",
     "4" = "4.13.0",
     "3" = "3.5.17"
